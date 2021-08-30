bbac648c159490c22575caabaaca5832ab4a8ceb
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-08-30 15:44:05 -0400

    fix constants file name

--- src/components/Desktop/index.tsx
+++ src/components/Desktop/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./index.module.scss";
-import APPLICATIONS from '../../contants';
+import APPLICATIONS from '../../constants';
 
 interface IDesktop {
   children: React.ReactNode;
---
+++ src/constants.ts
@@ -0,0 +1,44 @@
+interface IApplications {
+    name: string;
+    src?: string;
+    icon: string;
+  id: number;
+}
+
+const PDF = "src/assets/icons/pdf.png";
+const CHROME = "src/assets/icons/chrome.png";
+
+const APPLICATIONS: IApplications[] = [
+  {
+    name: "CV",
+    icon: PDF,
+    src: "../src/assets/cv.pdf",
+    id: 1
+  },
+  {
+    name: "Portfolio",
+    icon: CHROME,
+    src: "https://anibal.cf",
+    id: 2
+  },
+  {
+    name: "Blog",
+    icon: CHROME,
+    src: "https://anibaldbxd.cf",
+    id: 2
+  },
+  {
+    name: "Linkedin",
+    icon: CHROME,
+    src: "https://www.linkedin.com/in/anibaloz/",
+    id: 2
+  },
+  {
+    name: "Twitter",
+    icon: CHROME,
+    src: "https://anibaldbxd.cf",
+    id: 2
+  },
+];
+
+export default APPLICATIONS;
