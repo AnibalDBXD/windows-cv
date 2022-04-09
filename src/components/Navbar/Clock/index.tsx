6c2ba0000775b10a17c647763c64320060dea9c1
AnibalDBXD <olivaresluisanibal@gmail.com>
2022-04-09 13:07:48 -0400

    Update Clock time each half minute

--- src/components/Navbar/Clock/index.tsx
+++ src/components/Navbar/Clock/index.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./index.module.scss";
 
+const TIME = 30 * 1000;
+
 const Clock = (): JSX.Element => {
-  const currentDate = new Date();
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(new Date());
+    }, TIME);
+
+    return (): void => {
+      clearInterval(interval);
+    };
+  }, []);
+
 
   const currentHour = currentDate.toLocaleTimeString(undefined, { timeStyle: "short" });
   return (
